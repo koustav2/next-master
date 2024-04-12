6ec318a8ae858f23dc2361b4ff79700cdfc3758c
koustav2 <koustavm.aeie208@nsec.ac.in>
2024-04-12 09:34:35 +0530

    toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils"
+import { Toaster } from 'react-hot-toast'
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -29,6 +30,10 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+        />
       </body>
     </html>
   );
